f2a14a4aa84ed2e9e48c7c08ea8967e9a11626af
Cherie Davis <cheriedavis126@gmail.com>
2025-02-04 17:24:32 -0500

    updating main.ts file

--- Develop/client/src/main.ts
+++ Develop/client/src/main.ts
@@ -35,7 +35,7 @@ API Calls
 */
 
 const fetchWeather = async (cityName: string) => {
-  const response = await fetch('/api/weather/', {
+  const response = await fetch("http://localhost:5000/api/weather", {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
