5de9dfcc7a46c09783c6af3fed68adaff8fc3ae8
Cherie Davis <cheriedavis126@gmail.com>
2025-02-08 21:04:32 -0500

    debugging render package.json path

---
+++ Develop/build/server/src/server.js
@@ -0,0 +1,21 @@
+import dotenv from "dotenv";
+import express from "express";
+import { fileURLToPath } from "url";
+import path from "path";
+dotenv.config();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+// Import the routes
+import routes from './routes/index.js';
+const app = express();
+const PORT = process.env.PORT || 3001;
+console.log('App is running on port: ' + PORT);
+// TODO: Serve static files of entire client dist folder
+app.use(express.static(path.join(__dirname, "../../client/dist")));
+// TODO: Implement middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// TODO: Implement middlewapp.use(express.json());
+app.use(routes);
+// Start the server on the port
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
